ab9bf555ab27ed6ece733186f26e4f317b42ac4c
guzev <guzev1997@gmail.com>
2019-10-09 20:07:25 +0300

    All fixes finished

--- test/test_game.py
+++ test/test_game.py
@@ -1,8 +1,10 @@
 import unittest
 
+
 from src.game import find_all_pos_in_word
 from src.game import step_of_game
 from src.game import change_chars_in_positions
+
 
 class TestGame(unittest.TestCase):
 
@@ -19,15 +21,16 @@ class TestGame(unittest.TestCase):
         Test that functions changs letters in certian positions
         :return: changed word
         """
-        self.assertEqual(change_chars_in_positions('aaa', [0, 1, 2], 'b'), 'bbb')
+        self.assertEqual(change_chars_in_positions('aaa',
+                                                   [0, 1, 2], 'b'), 'bbb')
 
     def test_step_of_game(self):
         """
         Test that one choice of player works right
         :return: changed word
         """
-        #enter the letter y
-        #self.assertEqual(step_of_game('****', 'yeah'), 'y***')
+#       enter the letter y
+#       self.assertEqual(step_of_game('****', 'yeah'), 'y***')
 
 
 if __name__ == '__main__':
